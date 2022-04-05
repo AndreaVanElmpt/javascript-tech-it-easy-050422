93b76a18b18f9123d508498d939b8413b189c7e7
AndreaVanElmpt <oibibio@gmail.com>
2022-04-05 17:06:10 +0200

    huiswerk-met-sam'

---
+++ opdract1.js
@@ -0,0 +1,34 @@
+//pseudo-code
+
+//1a:
+//
+//
+
+
+
+const tvNames = inventory.map ((tv) => {
+    return tv.type
+})
+console.log(tvNames)
+
+//1b:
+// 1. gebruik de filter-methoths
+// 2. Filter uitkomst
+
+const tvSold = inventory.filter((tvSoldOut) => {
+    return tvSoldOut.originalStock === tvSoldOut.sold
+})
+
+console.log(tvSold)
+
+//1c:
+
+// 1. gebruik de filter methode
+// 2. Via options van het object options om bij ambilight te komen
+// 3. Filter uitkomst
+
+const tvAmbiLight =  inventory.filter((tvAmbi) => {
+    return tvAmbi.options.ambiLight === true
+})
+
+console.log(tvAmbiLight)
